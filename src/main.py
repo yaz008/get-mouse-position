4ce3acdd09a626a3bc0a03e549e39a81b4b27a02
yaz008 <yaz008.yaz008@yandex.ru>
2024-06-25 15:25:48 +0300

    Add exit key

--- src/main.py
+++ src/main.py
@@ -3,10 +3,14 @@ from pynput.mouse import Controller
 from typing import Callable
 
 mouse: Controller = Controller()
-def getter(toggle: Key | KeyCode) -> Callable[[Key | KeyCode], None]:
-    def wrapper(pressed: Key | KeyCode) -> None:
-        if pressed == toggle:
+def getter(toggle: Key | KeyCode,
+           exit_key: Key | KeyCode = Key.esc
+           ) -> Callable[[Key | KeyCode], None]:
+    def wrapper(pressed_key: Key | KeyCode) -> None:
+        if pressed_key == toggle:
             print(mouse.position)
+        if pressed_key == exit_key:
+            exit()
     return wrapper
 
 with Listener(on_press=getter(toggle=KeyCode(char='t'))) as listener:

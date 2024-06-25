c3812d9e46a4a9f9b4d3ed188b75e8120514e7a0
yaz008 <yaz008.yaz008@yandex.ru>
2024-06-25 14:59:06 +0300

    Add getter
    Replacement of get_possition

--- src/main.py
+++ src/main.py
@@ -1,10 +1,13 @@
 from pynput.keyboard import Listener, Key, KeyCode
 from pynput.mouse import Controller
+from typing import Callable
 
 mouse: Controller = Controller()
-def get_position(pressed: Key | KeyCode) -> None:
-    if isinstance(pressed, KeyCode) and pressed.char == 't':
-        print(mouse.position)
+def getter(toggle: Key | KeyCode) -> Callable[[Key | KeyCode], None]:
+    def wrapper(pressed: Key | KeyCode) -> None:
+        if pressed == toggle:
+            print(mouse.position)
+    return wrapper
 
-with Listener(on_press=get_position) as listener:
+with Listener(on_press=getter(toggle=KeyCode(char='t'))) as listener:
     listener.join()
